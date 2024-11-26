f9f34e10f2226d1a8a85d0725e4b94d242148e59
roseannebc06 <roseannebarlizocaagbay@gmail.com>
2024-11-26 16:20:18 +0800

    Create Lamp.java

---
+++ 01LampActivity/Lamp.java
@@ -0,0 +1,14 @@
+public class Lamp
+{
+	public String name;
+	
+	boolean isOn;
+	
+	void turnOn(){
+		System.out.println("Lights on? " + isOn);
+	}
+	
+	void turnOff(){
+		System.out.println("Lights on? " + isOn);
+	}
+}
