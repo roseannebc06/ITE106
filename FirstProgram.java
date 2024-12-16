615505015140509db28daf0fda6172ffe4738fa8
roseannebc06 <roseannebarlizocaagbay@gmail.com>
2024-12-16 13:21:03 +0800

    Create FirstProgram.java

---
+++ FirstProgram.java
@@ -0,0 +1,14 @@
+public class FirstProgram
+{
+	public static void main(String[]args) {
+		int base;
+		int height;
+		int area;
+		
+		base = 5;
+		height = 5;
+		
+		area = 1/2 * base * height;
+		System.out.println("Area = " + area);
+	}
+}
